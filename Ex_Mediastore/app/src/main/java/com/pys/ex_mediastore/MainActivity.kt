1418e25953e9973d790eec67327c0725398fe941
yeonsoosoo <lovepys11@naver.com>
2023-06-01 16:46:39 +0900

    카메라 연동

--- Ex_Mediastore/app/src/main/java/com/pys/ex_mediastore/MainActivity.kt
+++ Ex_Mediastore/app/src/main/java/com/pys/ex_mediastore/MainActivity.kt
@@ -1,11 +1,102 @@
 package com.pys.ex_mediastore
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.pys.ex_mediastore.databinding.ActivityMainBinding
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_PERMISSION_CAMERA_STORAGE = 201
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mFile: File
+    private lateinit var cameraImageUri: Uri
+    private lateinit var intentCamera: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        val resolver = applicationContext.contentResolver //MediaStore
+
+        mBinding.btnCamera.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    private fun takePhoto() {
+        intentCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val currentDate = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+
+        val fileName = "camera_$currentDate"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        var file: File? = null
+        try {
+            file = File.createTempFile(
+                fileName,
+                ".jpg",
+                storageDir
+            )
+            mFile = file
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return
+        }
+
+        // File 객체 URI 얻기
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            cameraImageUri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", file)
+        } else {
+            cameraImageUri = Uri.fromFile(file)
+        }
+        intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, cameraImageUri)
+
+        // 카메라 intent 포함
+        if (CommonUtil.checkPermission(this, REQUEST_PERMISSION_CAMERA_STORAGE)) {
+            startCameraResult.launch(intentCamera)
+        }
+    }
+
+    private val startCameraResult: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                Log.d("카메라 연동", " 성공")
+            }
+        }
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_PERMISSION_CAMERA_STORAGE) {
+            if(grantResults.size > 1
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                CommonUtil.showMoveSettingDialog(this, R.string.message_camera_external_read_permission)
+            }
+        }
     }
 }
