e15bb0e732852e631a3c5aabe2047b3a35316161
yeonsoosoo <lovepys11@naver.com>
2023-07-14 12:41:58 +0900

    영화진흥원 API 활용 예제

---
+++ Ex_KobisApi/app/src/main/java/com/pys/ex_kobisapi/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pys.ex_kobisapi.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pys.ex_kobisapi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
