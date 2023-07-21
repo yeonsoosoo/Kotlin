e45afff52a636f30d7208f81ee420903f3e41143
yeonsoosoo <lovepys11@naver.com>
2023-07-21 16:35:02 +0900

    viewModel 및 repository 셋팅

--- Ex_KobisApi/app/src/main/java/com/pys/ex_kobisapi/ui/viewmodel/MainViewModel.kt
+++ Ex_KobisApi/app/src/main/java/com/pys/ex_kobisapi/ui/viewmodel/MainViewModel.kt
@@ -1,4 +1,12 @@
 package com.pys.ex_kobisapi.ui.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import com.pys.ex_kobisapi.data.repository.MainRepository
+
+class MainViewModel(private val repository : MainRepository) : ViewModel() {
+
+    // api 호출
+    fun getMovieInfo() {
+
+    }
 }
--- Ex_KobisApi/app/src/main/java/com/pys/ex_kobisapi/ui/viewmodel/viewmodelFactory/MainViewModelFactory.kt
+++ Ex_KobisApi/app/src/main/java/com/pys/ex_kobisapi/ui/viewmodel/viewmodelFactory/MainViewModelFactory.kt
@@ -1,4 +1,13 @@
 package com.pys.ex_kobisapi.ui.viewmodel.viewmodelFactory
 
-class MainViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.pys.ex_kobisapi.data.repository.MainRepository
+import com.pys.ex_kobisapi.ui.viewmodel.MainViewModel
+
+class MainViewModelFactory(private val repository: MainRepository) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(repository) as T
+    }
 }
